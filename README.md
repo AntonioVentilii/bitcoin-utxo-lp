76d80e24b60c32f87c7fb36c5b4683176796adad
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-01-21 16:39:29 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Bitcoin UTXO Linear Problem Solving
---
+++ tests/test_smoke.py
@@ -0,0 +1,5 @@
+from bitcoin_utxo_lp import __version__
+
+def test_version_exists():
+    assert isinstance(__version__, str)
+    assert __version__
