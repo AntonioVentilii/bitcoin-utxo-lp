0a086bd93df796d23572f3eadf0aec2c3bca22e2
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-01-22 09:35:26 +0100

    dfx and kybra

---
+++ canisters/backend/main.py
@@ -0,0 +1,83 @@
+from typing import TypedDict
+
+from kybra import (
+    Record,
+    StableBTreeMap,
+    Vec,
+    ic,
+    init,
+    nat64,
+    post_upgrade,
+    pre_upgrade,
+    query,
+    update,
+    void,
+)
+
+
+class StableStorage(TypedDict):
+    entries: Vec["Entry"]
+
+
+class Entry(Record):
+    key: str
+    value: nat64
+
+
+stable_storage = StableBTreeMap[str, Vec[Entry]](
+    memory_id=0, max_key_size=100, max_value_size=100
+)
+
+entries: dict[str, nat64] = {}
+
+
+message: str = "Hello!"
+
+
+@init
+def init_() -> void:
+    ic.print("init_")
+
+    stable_storage.insert("entries", [])
+
+
+@pre_upgrade
+def pre_upgrade_() -> void:
+    ic.print("pre_upgrade_")
+
+    stable_storage.insert(
+        "entries",
+        list(map(lambda item: {"key": item[0], "value": item[1]}, entries.items())),
+    )
+
+
+@post_upgrade
+def post_upgrade_() -> void:
+    ic.print("post_upgrade_")
+
+    stable_entries = stable_storage.get("entries")
+
+    if stable_entries is not None:
+        for stable_entry in stable_entries:
+            entries[stable_entry["key"]] = stable_entry["value"]
+
+
+@update
+def set_entry(entry: Entry) -> void:
+    entries[entry["key"]] = entry["value"]
+
+
+@query
+def get_entries() -> Vec[Entry]:
+    return [{"key": key, "value": entries[key]} for key in entries.keys()]
+
+
+@query
+def get_message() -> str:
+    return message
+
+
+@update
+def set_message(new_message: str) -> void:
+    global message
+    message = new_message
